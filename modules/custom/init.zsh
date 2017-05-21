c6303d0deebf66ff4de5c0b95eaa75355836a2ee
Benjamin Ryzman <benjamin.ryzman@intercloud.fr>
2017-05-21 22:07:26 +0200

    Add aliases definition at login

--- modules/custom/init.zsh
+++ modules/custom/init.zsh
@@ -5,4 +5,11 @@
 # any custom stuff should go here.
 # ensure that 'custom' exists in the zmodules array in your .zimrc
 
-
+zim_login=${ZDOTDIR:-${HOME}}/.zim/modules/custom/login
+if [[ -o login && -d ${zim_login} ]]
+then
+    for f in ${zim_login}/*.zsh
+    do
+        source $f
+    done
+fi
