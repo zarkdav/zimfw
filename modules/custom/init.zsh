f7be98d8f602e3cfb9cec8cc72ccc02d5cedf6a5
Benjamin Ryzman <benjamin@ryzman.fr>
2020-04-12 18:17:28 +0200

    Move aliases initialisation to the proper location

--- modules/custom/init.zsh
+++ modules/custom/init.zsh
@@ -5,45 +5,6 @@
 # any custom stuff should go here.
 # ensure that 'custom' exists in the zmodules array in your .zimrc
 
-if (( $+commands[vim] ))
-then
-    export VISUAL=vim
-    alias vi="vim"
-
-    vim_version=$(vim --version | head -1 | grep -o '[0-9]\.[0-9]')
-    if [[ $+commands[vimpager] -eq 1 && $vim_version -ge 8.0 ]]
-    then
-        export PAGER=vimpager
-        alias less=$PAGER
-        alias zless=$PAGER
-    fi
-fi
-
-if [[ -x grabssh ]]
-then
-    alias screen="grabssh && screen"
-fi
-
-alias mkdir='mkdir -p -v'
-alias mv='timeout 8 mv -iv'
-alias rm='timeout 3 rm -Iv --one-file-system'
-alias cp="cp -i"
-alias ll="ls -l"
-alias la="ls -a"
-alias lh="ls -lh"
-alias lsd='ls -ld *(-/DN)'
-alias df="df -h"
-alias ip="ip -c"
-
-
-alias lsdir="for dir in *;do;if [ -d \$dir ];then;du -hsL \$dir 2>/dev/null;fi;done"
-
-alias -s log="tail -f"
-
-alias -s pem="openssl x509 -text -noout -in"
-alias -s req="openssl req -text -in"
-
-ssh_key_manage
 
 zim_login=${ZDOTDIR:-${HOME}}/.zim/modules/custom/login
 if [[ -o login && -d ${zim_login} ]]
